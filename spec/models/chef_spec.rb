1ccdb57240a139e6c0f8d8a028ddbd9b4fae1188
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-19 12:46:01 -0600

    Create model spec for unique_ingredients method

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,32 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe 'instance methods' do
+    before :each do
+      @chef = Chef.create!(name: "Gordon Ramsay")
+      @chef2 = Chef.create!(name: "Rachel Ray")
+
+      @dish1 = Dish.create!(name: "Beef Wellington", description: "Delicious beef dish", chef_id: @chef.id)
+      @dish2 = Dish.create!(name: "Spaghetti", description: "Noodles & red sauce", chef_id: @chef.id)
+
+      @beef = Ingredient.create!(name: "Ground beef", calories: 500)
+      @pate = Ingredient.create!(name: "Pate", calories: 300)
+      @mushroom = Ingredient.create!(name: "Mushrooms", calories: 150)
+      @pasta = Ingredient.create!(name:"Noodles", calories: 250)
+
+      DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @beef.id)
+      DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @pate.id)
+      DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @mushroom.id)
+      DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @pasta.id)
+      DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @mushroom.id)
+      DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @beef.id)
+    end
+
+    describe '#unique_ingredients' do
+      it 'can create a unique list of all ingredients a chef uses in all of their dishes' do
+        expect(@chef.unique_ingredients).to eq([@beef, @pate, @mushroom, @pasta])
+      end
+    end
+  end
 end
